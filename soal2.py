ee144b9d535759500a2a2a6ec738621227d158dc
Nurul Qofifah Audyningrum <66819730+audyningrum27@users.noreply.github.com>
2020-11-17 16:35:47 +0800

    HW_TeknofestWeek2

--- soal2.py
+++ soal2.py
@@ -1,13 +1,13 @@
-print("Klub A :", end = " ")
+print("Klub A :", end=" ")
 a = input()
-print("Klub B :", end = " ")
+print("Klub B :", end=" ")
 b = input()
 
 i = 0
 arr = []
 while i >= 0:
     i += 1
-    print(f"Pertandingan {i} :", end = " ")
+    print(f"Pertandingan {i} :", end=" ")
     n = input().split()
     arr.append(n)
     if int(n[0] or n[1]) <= -1:
@@ -16,17 +16,17 @@ while i >= 0:
 
 j = 0
 for n in arr:
-    j+=1
+    j += 1
     na = int(n[0])
     nb = int(n[1])
-    if na > nb:
-        print(f"Hasil {j} : {a}", end = " ")
-    elif (na or nb) < 0:
+    if (((na or nb) or (na and nb))) < 0:
         print("Pertandingan selesai")
         break
+    elif na > nb:
+        print(f"Hasil {j} : {a}", end=" ")
     elif nb > na:
-        print(f"Hasil {j} : {b}", end = " ")
+        print(f"Hasil {j} : {b}", end=" ")
     elif na == nb:
-        print(f"Hasil {j} : Draw", end = " ")
+        print(f"Hasil {j} : Draw", end=" ")
 
     print("")
